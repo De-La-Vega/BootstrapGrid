e07f3f1aad5e9d0cc729b942c0a93318690b5c32
Vitaliy Kirenkov <vkirenk2@ru.ibm.com>
2017-04-07 16:55:54 +0300

    fixed bug with wrong position, when layer in folder

--- Bootstrap grid.sketchplugin/Contents/Sketch/BootstrapGrid.js
+++ Bootstrap grid.sketchplugin/Contents/Sketch/BootstrapGrid.js
@@ -6,7 +6,7 @@ var overlayColor = "#ff0000";
 /**
  * Creating group.
  */
-function addGroup () {
+function createGroup () {
     return MSLayerGroup.new();
 }
 
@@ -19,7 +19,7 @@ function addShape (x, y, width, height, index) {
 
     shapeGroup.setName("Col " + index);
     shapeGroup.style().contextSettings().setOpacity(overlayOpacity);
-    // shapeGroup.setIsLocked(true); 
+    // shapeGroup.setIsLocked(true);
     var fill = shapeGroup.style().addStylePartOfType(0);
     fill.color = MSImmutableColor.colorWithSVGString(overlayColor);
 
@@ -39,38 +39,69 @@ function getLayerParams (frame) {
 }
 
 /**
+ * Get layers parent.
+ */
+var getParent = function(object) {
+    var objParentGroup = object.parentGroup();
+    var isParentArtboard = objParentGroup.isKindOfClass(MSArtboardGroup);
+    var isParentPage = objParentGroup.isKindOfClass(MSPage);
+    var result;
+
+    if (isParentArtboard || isParentPage) {
+        result = object;
+    } else {
+        result = getParent(objParentGroup);
+    }
+
+    return result;
+};
+
+/**
  * Plugin.
  */
 var onRun = function(context) {
+    //reference the Application
+    var app = [NSApplication sharedApplication];
+
     var target = context.selection;
     var targetCount = target.count();
     var document = context.document;
     var page = document.currentPage();
     var artboard = page.currentArtboard();
     var current = artboard || page;
-    var container = addGroup();
-    var overlays = [];
-
-    current.addLayers([container]);
-
+    
     if (targetCount == 0) {
-        log('No layers are selected.');
+        // log('No layers are selected.');
+
+        // show a message at the bottom of Sketch
+        // doc.showMessage("MyPlugin Finished!");
+
+        //send an alert message to the application
+        [app displayDialog: "Please, select layer for which yon want insert grid" withTitle: "No layers are selected!"];
     } else {
-        for (var i = 0; i < targetCount; i++) {
-            var layer = target[i];
+        var containersArr = [];
+
+        for (var selectionIndex = 0; selectionIndex < targetCount; selectionIndex++) {
+            var layer = getParent(target[selectionIndex]);
+            var container = createGroup();
             var params = getLayerParams(layer.frame());
             var colWidth = (params.width - gutterWidth * 22)/colCount;
             var indent = colWidth + gutterWidth * 2;
+            var overlays = [];
 
             for (var i = 1; i <= colCount; i++) {
                 var x = i == 1 ? params.posX : params.posX + indent * (i - 1);
                 var overlay = addShape(x, params.posY, colWidth, params.height, i);
                 overlays.push(overlay);
             }
-        }
-    };
 
-    container.setName('Grid Folder');
-    container.setIsLocked(true);
-    container.addLayers(overlays);
+            container.setName('Grid Folder ' + selectionIndex);
+            container.setIsLocked(true);
+            container.addLayers(overlays);
+
+            containersArr.push(container);
+        }
+
+        current.addLayers(containersArr);
+    };
 }
